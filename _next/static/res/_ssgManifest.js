4e1e2666f673685b7c543aee0b00a1fe64ff8243
Joseph Mearman <joseph@mearman.co.uk>
2024-10-20 22:28:16 +0100

    Updates

---
+++ _next/static/res/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fproduct\u002F[id]","\u002Fproducts\u002F[year]","\u002Fproducts\u002F[year]\u002F[month]","\u002Fproducts\u002F[year]\u002F[month]\u002F[day]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
